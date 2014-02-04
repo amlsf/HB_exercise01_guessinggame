3e9d0cdb37b599b86077e03edc6ce84333730545
Hackbright Student <info@hackbrightacademy.com>
2014-02-04 12:55:05 -0800

    Try except attempt doesn't let me CTRL-C out

--- guessing.py
+++ guessing.py
@@ -10,16 +10,17 @@ print "Try to guess my number."
 answer = randint(1,100) #randint is inclusive of 1 to 100
 print answer
 
-guess = int(raw_input("Your guess?"))
+while True:
+    try:
+        guess = int(raw_input("Your guess?"))    
 
-while guess != answer:
-    guess = int(guess)
-    if guess < answer:
-        print "Your guess is too low, try again"
-        guess = raw_input("Your guess?")
-    elif guess > answer:
-        print "Your guess is too high, try again"
-        guess = raw_input("Your guess?")
-    else: 
-        print "Well done, %s" % name,
-        print "You found my number in x tries!"
+        if guess < answer:
+            print "Your guess is too low, try again"
+        elif guess > answer:
+            print "Your guess is too high, try again"
+        else: 
+            print "Well done, %s" % name,
+            print "You found my number in x tries!"
+            break
+    except:
+        print "invalid input"
